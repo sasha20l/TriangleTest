ac559658306822ea5dd828810293423213360e6d
sasha <sasha21l@mail.ru>
2023-12-01 19:23:37 +0500

    Тестовое задание про треугольник.

---
+++ Triangle/TriangleT.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle
+{
+    internal class TriangleT
+    {
+        public string triangleType(double sideA, double sideB, double sideC)
+        {
+            double maxSide = 0;
+            double sidesToDegree = 0;
+
+            if (sideA > sideB)
+            {
+                if (sideA >= sideC)
+                {
+                    maxSide = sideA * sideA;
+                    sidesToDegree = ((sideB * sideB) + (sideC * sideC));
+                }
+                else if (sideA < sideC)
+                {
+                    maxSide = sideC * sideC;
+                    sidesToDegree = ((sideB * sideB) + (sideA * sideA));
+                }
+
+            }
+            else if (sideB >= sideC)
+            {
+                maxSide = sideB * sideB;
+                sidesToDegree = ((sideA * sideA) + (sideC * sideC));
+            }
+            else if (sideB < sideC)
+            {
+                maxSide = sideC * sideC;
+                sidesToDegree = ((sideB * sideB) + (sideA * sideA));
+            }
+            else if ((sideA == sideB) && (sideB != sideC))
+            {
+                if (sideA > sideC)
+                {
+                    maxSide = sideA * sideA;
+                    sidesToDegree = ((sideB * sideB) + (sideC * sideC));
+                }
+                else
+                {
+                    maxSide = sideB * sideB;
+                    sidesToDegree = ((sideA * sideA) + (sideC * sideC));
+                }
+            }
+            else return "Прямоугольный";
+
+            if (maxSide == sidesToDegree) return "Прямоугольный";
+            else if (maxSide < sidesToDegree) return "Остроугольный";
+            else return "Тупоугольный";
+        }
+    }
+}
---
+++ Triangle/bin/Debug/net6.0/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Triangle;
+
+Random rnd = new Random();
+TriangleT tr = new TriangleT();
+
+for (int i = 1; i < 30; i++)
+{
+    
+    
+    double a = rnd.Next();
+    double b = rnd.Next();
+    double c = rnd.Next();
+
+    Console.WriteLine($"{a} {b} {c}");
+    Console.WriteLine(tr.triangleType(a,b,c));
+    
+}
+Console.ReadLine();
+
